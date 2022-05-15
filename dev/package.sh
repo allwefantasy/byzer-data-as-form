5765c926749cf98c70d550c8be93c4272551abf1
jiachuan.zhu <jiachuan.zhu@kyligence.io>
2022-05-15 16:26:08 +0800

    Change jar file name in package.sh

--- dev/package.sh
+++ dev/package.sh
@@ -20,7 +20,7 @@ mvn clean package -DskipTests -Pshade -pl ${v}-bin
 
 mkdir -p release/${v}-bin/lib release/${v}-bin/bin
 cp -r config release/${v}-bin/
-cp -r ${v}-bin/target/${v}-bin_2.11-${VERSION}.jar release/${v}-bin/lib
+cp -r ${v}-bin/target/${v}-bin_2.12-${VERSION}.jar release/${v}-bin/lib
 cp dev/start.sh release/${v}-bin/bin
 cd release
 tar czvf ${v}-bin.tar.gz ${v}-bin
