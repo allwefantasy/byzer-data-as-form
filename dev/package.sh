c9351d190c81afe4195847c9b47b9801f1513798
WilliamZhu <allwefantasy@gmail.com>
2022-10-05 12:05:12 +0800

    update package script

--- dev/package.sh
+++ dev/package.sh
@@ -43,6 +43,7 @@ mkdir -p release/${v}-bin/lib release/${v}-bin/bin
 cp -r config release/${v}-bin/
 cp -r ${v}-bin/target/byzer-data-as-api-bin_2.12-${VERSION}.jar release/${v}-bin/lib
 cp dev/*.sh release/${v}-bin/bin
+cp -r form release/${v}-bin/form
 cd release
 tar czvf ${v}-bin.tar.gz ${v}-bin
 cd -
