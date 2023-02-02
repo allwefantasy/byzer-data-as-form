6645cc2bf7843e5a0a6205ac2e0c9d2436efcfd0
WilliamZhu <allwefantasy@gmail.com>
2023-02-02 12:21:09 +0800

    update

--- dev/start.sh
+++ dev/start.sh
@@ -1,1 +1,15 @@
-java -cp .:lib/* tech.mlsql.app_runtime.plugin.App
+   # set BYZER_HOME
+if [ -z BYZER_FORM_HOME ];then
+   export BYZER_FORM_HOME=$(cd -P -- "$(dirname -- "$0")"/../ && pwd -P)
+fi
+
+nohup java -cp .:lib/* tech.mlsql.app_runtime.plugin.App >> ${BYZER_FORM_HOME}/logs/byzer-form.out &
+echo $! >> ${BYZER_FORM_HOME}/pid
+
+sleep 3
+
+[ ! -f "${BYZER_FORM_HOME}/pid" ] && quit "Byzer Form start failed, check via log: ${BYZER_FORM_HOME}/logs/byzer-form.out."
+
+PID=$(cat ${BYZER_FORM_HOME}/pid)
+CUR_DATE=$(date "+%Y-%m-%d %H:%M:%S")
+echo $CUR_DATE" new Byzer Form process pid is "$PID >> ${BYZER_FORM_HOME}/logs/serviceframework-0.log
