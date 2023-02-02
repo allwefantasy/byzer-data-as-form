8f668cdcd80f8312050369c8b542b9d2e085ff7f
WilliamZhu <allwefantasy@gmail.com>
2023-02-02 11:53:39 +0800

    update

--- dev/package.sh
+++ dev/package.sh
@@ -17,7 +17,7 @@ then
   for item in web-platform app_runtime_with_db user-system ar_runtime_web_console
   do
       cd ${item}
-      git pull origin master
+      # git pull origin master
       echo "installing ${item}"
       mvn -DskipTests  install
       cd ..
@@ -64,6 +64,13 @@ cd release
 mv ${v}-bin ${v}-bin-${VERSION}
 tar czvf ${v}-bin-${VERSION}.tar.gz ${v}-bin-${VERSION}
 cd -
+
+TIME=$(date +%F)
+if [[ "${UPLOAD}" == "true" ]];then
+  curl \
+    -F "byzer-data-as-form-bin-${VERSION}.tar.gz=@/Users/allwefantasy/projects/byzer-data-as-form/release/byzer-data-as-form-bin-${VERSION}.tar.gz" \
+  "${BYZER_UPLOADER_URL}&pathPrefix=latest/${TIME}"
+fi
 
 
 
