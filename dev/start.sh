a1345fad63c3d9cfe423eae5b39007d5ef1b62a3
WilliamZhu <allwefantasy@gmail.com>
2023-09-10 15:05:17 +0800

    update

--- dev/start.sh
+++ dev/start.sh
@@ -1,8 +1,18 @@
    # set BYZER_HOME
-if [ -z BYZER_FORM_HOME ];then
+if [ -z $BYZER_FORM_HOME ];then
    export BYZER_FORM_HOME=$(cd -P -- "$(dirname -- "$0")"/../ && pwd -P)
 fi
 
+cd BYZER_FORM_HOME
+
+if [[ -f pid ]];then
+  echo "Byzer Form is running, stop it first"
+  kill -9 `cat pid`
+fi
+
+sleep 3
+
+echo "start Byzer Form"
 nohup java -cp .:lib/* tech.mlsql.app_runtime.plugin.App >> ${BYZER_FORM_HOME}/logs/byzer-form.out &
 echo $! >> ${BYZER_FORM_HOME}/pid
 
